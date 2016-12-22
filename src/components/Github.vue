073f73cc364a6f91221f53b2a8911f290e3eb4a1
peterfei <peterfeispace@gmail.com>
2016-12-22 10:10:46 +0800

    编写页面

---
+++ src/components/Github.vue
@@ -0,0 +1,31 @@
+<script>
+	export default {
+		name:'Github',
+		data: function(){
+			return {
+				fullRepoName:''
+			}
+		}
+	}
+</script>
+<template>
+	<div id="container" class="container">
+		<div class="row">
+			<legend >
+				<h2>
+					<span class="mega-octicon octicon-mark-github">
+						文件浏览器
+					</span>
+				</h2>
+			</legend>
+
+			<form  id="changeRepoForm" class="form-inline" @submit.prevent="changeRepo()">
+				<div class="form-group">
+					<label for="fullRepoName">Repo 名称</label>
+					<input type="text" class="form-control" id="fullRepoName" v-model="fullRepoName" style="width:300px;">
+				</div>
+				<button class="btn btn-primary">获&nbsp;取</button>
+			</form>
+		</div>
+	</div>
+</template>
